765d67915df1b8e328d706024400189103f78a89
panhb <391400311@qq.com>
2015-09-24 16:36:02 +0800

    oneapm

--- oneapm.js
+++ oneapm.js
@@ -1,6 +1,5 @@
-var config = require('./config');
 /**
- * New Relic agent configuration.
+ * OneAPM agent configuration.
  *
  * See lib/config.defaults.js in the agent distribution for a more complete
  * description of configuration variables and their potential values.
@@ -9,17 +8,20 @@ exports.config = {
   /**
    * Array of application names.
    */
-  app_name: [config.name],
+  app_name : ['w2r'],
   /**
-   * Your New Relic license key.
+   * Your OneAPM license key.
    */
-  license_key: config.newrelic_options.license_key,
-  logging: {
+  license_key : 'BglXAAcKAQU79f2GH1YWDQ8YWk7991JVCh4FCQgEH238cwMBTlNXGAYGac44CFMfBwEUAgM=',
+  logging : {
     /**
-     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * Level at which to log. 'trace' is most useful to OneAPM when diagnosing
      * issues with the agent, 'info' and higher will impose the least overhead on
      * production applications.
      */
-    level: 'info'
+    level : 'info'
+  },
+  transaction_events: {
+        enabled: true
   }
-}
+};
